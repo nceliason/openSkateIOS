bdb86abc39b24b8787418a243ba8b495fc93c302
Nick Eliason <nceliason@mac.com>
2016-01-28 21:45:16 -0800

    Added a label and swiping gestures that change label text

--- openSkateIOS/ViewController.swift
+++ openSkateIOS/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let upSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        let downSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        
+        upSwipe.direction = .Up
+        downSwipe.direction = .Down
+        rightSwipe.direction = .Right
+        leftSwipe.direction = .Left
+        
+        view.addGestureRecognizer(upSwipe)
+        view.addGestureRecognizer(downSwipe)
+        view.addGestureRecognizer(rightSwipe)
+        view.addGestureRecognizer(leftSwipe)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPress(sender: UIButton) {
+        label.text = "new text"
+    }
 
+    @IBAction func handleSwipes(sender: UISwipeGestureRecognizer) {
+        if (sender.direction == .Up) {
+            label.text = "up"
+        }
+        else if (sender.direction == .Down) {
+            label.text = "down"
+        } else if (sender.direction == .Left){}
+        else if (sender.direction == .Right){}
+    }
 }
 
